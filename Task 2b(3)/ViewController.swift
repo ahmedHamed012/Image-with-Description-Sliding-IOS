4e8cccf609815853bc22ed482f9717177ea4f024
Ahmed Hamed <ah0120615@gmail.com>
2023-10-24 10:52:37 +0300

    Initial Commit

---
+++ Task 2b(3)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2b(3)
+//
+//  Created by Ahmed Hamed on 24/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
