d66167098858aad5cc1903aba34e5a5e071d8cc6
Ahmed Hamed <ah0120615@gmail.com>
2023-10-24 17:59:35 +0300

    Updating Functionality

--- Task 2b(3)/ViewController.swift
+++ Task 2b(3)/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageShow: UIImageView!
+    @IBOutlet weak var imageInfo: UILabel!
+    struct Gallery {
+        var imageName: String
+        var imageDesc: String
+    }
+    let destinations : [Gallery] = [Gallery(imageName: "img-1", imageDesc: "Egypt  is Very Old"),Gallery(imageName: "img-3", imageDesc: "Egypt Has Many Places To Visit !"),Gallery(imageName: "img-2", imageDesc: "Pyramids Is one Of The Most Beautiful Places !"),Gallery(imageName: "img-4", imageDesc: "Lets Take a tour around Egypt!")]
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageInfo.text = "Welcome To EGYPT !"
     }
-
-
+    @IBAction func nxtBtn(_ sender: Any) {
+            imageShow.image = UIImage(named: destinations[counter].imageName)
+            imageInfo.text = destinations[counter].imageDesc
+        counter = (counter+1) % destinations.count
+    }
+    
+    @IBAction func prevBtn(_ sender: Any) {
+        imageShow.image = UIImage(named: destinations[counter].imageName)
+        imageInfo.text = destinations[counter].imageDesc
+        counter = (counter-1+destinations.count) % destinations.count
+    }
 }
-
